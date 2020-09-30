9200a81711c7612afa3a685d735531f05aac02ff
Mcl <mixool0204@gmail.com>
2020-09-30 11:38:14 +0800

    rename

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-nothing $(echo LXMgMC4wLjAuMCAtcAo= | base64 -d) ${PORT:=3000} $(echo LWsK | base64 -d) ${ONE:=ENO} \
+yingsuo $(echo LXMgMC4wLjAuMCAtcAo= | base64 -d) ${PORT:=3000} $(echo LWsK | base64 -d) ${ONE:=ENO} \
 $(echo LW0K | base64 -d) ${TWO:=$(echo cmM0LW1kNQo= |base64 -d)} $(echo LS1wbHVnaW4K | base64 -d) \
-onthing $(echo LS1wbHVnaW4tb3B0cwo= | base64 -d) "$(echo c2VydmVyO3BhdGg9Cg== | base64 -d)${THREE:=$(echo L3NzcGF0aAo= | base64 -d)}"  >/dev/null 2>&1
+suoying $(echo LS1wbHVnaW4tb3B0cwo= | base64 -d) "$(echo c2VydmVyO3BhdGg9Cg== | base64 -d)${THREE:=$(echo L3NzcGF0aAo= | base64 -d)}"  >/dev/null 2>&1
